2cd18302e8025ab199cd9d7cabc2d604ce4d922d
moehim123 <moehimmat@gmail.com>
2024-04-03 03:34:31 +1100

    Function to print command line instructions

--- user_upload.php
+++ user_upload.php
@@ -26,3 +26,14 @@ function validateEmail($email){
     return filter_var($email,FILTER_VALIDATE_EMAIL); 
 } 
 
+//function to print the command line instructions  
+function commandlineinstruct(){
+    echo "--file [csv file name] – this is the name of the CSV to be parsed"; 
+    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)"; 
+    echo "--dry_run – this will be used with the --file directive in case we want to run the script but not insert
+    into the DB. All other functions will be executed, but the database won't be altered";   
+    echo "-u – MySQL username"; 
+    echo "-p – MySQL password"; 
+    echo "-h – MySQL host"; 
+    
+}
